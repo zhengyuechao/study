a2d1a382136ff357609006894d6bbd763847b8a7
zhengyuechao <zhengyuechao@senyint.com>
2020-02-25 16:48:48 +0800

    tests

--- springDesign/src/test/java/com/study/SpringDesignApplicationTests.java
+++ springDesign/src/test/java/com/study/SpringDesignApplicationTests.java
@@ -1,13 +1,34 @@
 package com.study;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.study.demo.springPublishEvent.MyService;
+import com.study.entity.OrderInfo;
+import com.study.service.buy.OrderService;
 
 @SpringBootTest
 class SpringDesignApplicationTests {
 
+	/*
+	 * @Test void contextLoads() { }
+	 */
+	
+	@Autowired
+	MyService myservice;
+
+	@Autowired
+	OrderService orderService;
+	
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		myservice.read("hello world");
+		OrderInfo o =new  OrderInfo("20191231","phone","1000$");
+		orderService.prepareOrder(o, "promotion3");
+		//personService.sayHello();
 	}
+	
 
 }
