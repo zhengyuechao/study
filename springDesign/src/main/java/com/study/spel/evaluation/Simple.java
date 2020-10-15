cb2ff6496acc1724620619167700d8945e1f6dbe
zhengyuechao <zhengyuechao@senyint.com>
2020-10-15 09:44:39 +0800

    base

---
+++ springDesign/src/main/java/com/study/spel/evaluation/Simple.java
@@ -0,0 +1,15 @@
+package com.study.spel.evaluation;
+
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		ExpressionParser parser = new SpelExpressionParser();
+		Expression exp = parser.parseExpression("'Hello World'.concat('!')"); 
+		String message = (String) exp.getValue();
+		System.out.println(message);
+	}
+}
