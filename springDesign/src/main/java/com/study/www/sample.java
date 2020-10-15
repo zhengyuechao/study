49f62f4dce2d3dde16a2cf8fa1fd5a921e15bd9c
zhengyuechao <zhengyuechao@senyint.com>
2020-10-15 10:26:36 +0800

    modify config

---
+++ springDesign/src/main/java/com/study/www/sample.java
@@ -0,0 +1,15 @@
+package com.study.www;
+
+public class sample {
+	
+	/**
+	 * 20201014 
+	 * JAVA
+	 * https://github.com/jtablesaw/tablesaw  NO
+	 * https://github.com/bbottema/simple-java-mail  NO
+	 * https://github.com/zhoutaoo/SpringCloud  no
+	 * JS
+	 * https://github.com/cnwhy/nzh  阿拉伯数字转汉字  
+	 */
+
+}
